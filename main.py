8deb35ccbe64a1a5eafac636960044a4b2e96e26
Lukas Rimkus <lukas.rimkus2001@gmail.com>
2022-08-06 21:33:02 +0300

    Add location api stuff to get the coordinates

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from unicodedata import name
 from dotenv import load_dotenv, dotenv_values
 import discord
 from discord.ext import commands
-import requests
-import json
+import aiohttp
 
 
 load_dotenv()
 
 DISCORD_TOKEN = ""
 GUILD = ""
+LOCATION_API_TOKEN = ""
 
 description = '''This bot is responsible for giving data regarding weather all over the world!'''
 intents = discord.Intents.default()
@@ -17,13 +18,14 @@ bot = commands.Bot(command_prefix='.', description=description, intents=intents)
 
 
 def set_constants_from_env_variables():
-    global DISCORD_TOKEN, GUILD
+    global DISCORD_TOKEN, GUILD, LOCATION_API_TOKEN
 
     env_variables = dotenv_values(".env")
 
     try:
         DISCORD_TOKEN = env_variables["DISCORD_TOKEN"]
         GUILD = env_variables["DISCORD_GUILD"]
+        LOCATION_API_TOKEN = env_variables["LOCATION_API_TOKEN"]
     except KeyError as e:
         print(f"Please set all environment variables. Error: {e}")
 
@@ -47,10 +49,45 @@ async def on_member_join(member):
         await guild.system_channel.send(to_send)
 
 
-@bot.command()
-async def hello(ctx, name="stranger"):
-    """Say hello"""
-    message = f"Hello, {name}!"
+@bot.command(name="hello")
+async def hello(ctx):
+    """Say hello to given person's name"""
+    message = f"Hello, {ctx.message.author}! Ask me anything about the weather!"
+    await ctx.send(message)
+
+
+async def get_coordinates_data(*location):
+    async with aiohttp.ClientSession() as session:
+        base_url = "http://api.positionstack.com/v1/forward"
+        location_string = " ".join(location)
+        params = {
+            "access_key": LOCATION_API_TOKEN,
+            "query": location_string,
+            "limit": 1,
+            "timezone_module": 1
+        }
+
+        async with session.get(base_url, params=params) as response:
+            if response.status == 200:
+                json_repsonse = await response.json()
+                data = json_repsonse["data"]
+                latitude, longitude, timezone_offset_sec = data[0]["latitude"], data[0]["longitude"], data[0]["timezone_module"]["offset_sec"]
+                print(latitude, longitude, timezone_offset_sec)
+            else:
+                response.raise_for_status()
+    
+    timezone_offset_h = int(timezone_offset_sec)//3600
+
+    return latitude, longitude, timezone_offset_h
+
+
+@bot.command(name="coordinates")
+async def get_coordinates(ctx, *location):
+    """Get the coordinates of the location"""
+
+    latitude, longitude, timezone_offset_h = await get_coordinates_data(*location)
+    message = f"Given location: {' '.join(location)}.\nLatitude = {latitude}, longitude = {longitude}, timezone = {timezone_offset_h} hours"
+
     await ctx.send(message)
 
 
