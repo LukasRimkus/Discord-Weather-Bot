c7dcdb61f31e8974b6e5dc643fdab16ee1edac5d
Lukas Rimkus <lukas.rimkus2001@gmail.com>
2022-08-06 15:39:22 +0300

    Remove the use of a class and add greetings to guild event

--- main.py
+++ main.py
@@ -7,51 +7,53 @@ import json
 
 load_dotenv()
 
-# description = '''This bot is responsible for giving data reagrding weather all over the world!'''
-# intents = discord.Intents.default()
-# intents.members = True
-# intents.dm_messages = True
-# bot = commands.Bot(command_prefix='.', description=description, intents=intents)
+DISCORD_TOKEN = ""
+GUILD = ""
+
+description = '''This bot is responsible for giving data regarding weather all over the world!'''
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='.', description=description, intents=intents)
 
 
-class WeatherBot(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+def set_constants_from_env_variables():
+    global DISCORD_TOKEN, GUILD
 
-        self.set_constants_from_env_variables()
-        self.description = '''This bot is responsible for giving data regarding weather all over the world!'''
+    env_variables = dotenv_values(".env")
 
-        self.run(self.DISCORD_TOKEN)
-
-    def set_constants_from_env_variables(self):
-        env_variables = dotenv_values(".env")
-
-        try:
-            self.DISCORD_TOKEN = env_variables["DISCORD_TOKEN"]
-            self.GUILD = env_variables["DISCORD_GUILD"]
-        except KeyError as e:
-            print(f"Please set all environment variables. Error: {e}")
-    
-    async def on_ready(self):
-        # made an assumption that bot joined only one server 
-        guild = discord.utils.get(self.guilds, name=self.GUILD)
-
-        if not guild:
-            print(f"There is no such GUILD = {self.GUILD}. Try again after updating GUILD.")
-            exit()
-        print("TEST")
-        # print(f'We have logged in as {bot.user} (ID: {bot.user.id}). The guild connected to is {guild.name} (ID: {guild.id})')
+    try:
+        DISCORD_TOKEN = env_variables["DISCORD_TOKEN"]
+        GUILD = env_variables["DISCORD_GUILD"]
+    except KeyError as e:
+        print(f"Please set all environment variables. Error: {e}")
 
 
-# @bot.command()
-# async def hello(ctx, name: str):
-#     """Say hello"""
-#     message = f"Hello, {name}!"
-#     await ctx.send(message)
+@bot.event
+async def on_ready():
+    guild = discord.utils.get(bot.guilds, name=GUILD)
 
+    if not guild:
+        print(f"There is no such GUILD = {GUILD}. Try again after updating GUILD.")
+        exit()
+
+    print(f'We have logged in as {bot.user} (ID: {bot.user.id}). The guild connected to is {guild.name} (ID: {guild.id})')
+
+
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        to_send = f'Welcome {member.mention} to {guild.name}!'
+        await guild.system_channel.send(to_send)
+
+
+@bot.command()
+async def hello(ctx, name="stranger"):
+    """Say hello"""
+    message = f"Hello, {name}!"
+    await ctx.send(message)
 
 
 if __name__ == "__main__":
-    bot = WeatherBot(command_prefix='.', intents=discord.Intents.default())
-
-    # bot.run(bot.DISCORD_TOKEN)
+    set_constants_from_env_variables()
+    bot.run(DISCORD_TOKEN)
