26b3ad2c3e9915e39a7da4acf96fbb3d1c826cbb
Lukas Rimkus <lukas.rimkus2001@gmail.com>
2022-08-07 14:00:37 +0300

    Add error handling

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ bot = commands.Bot(command_prefix='.', description=description, intents=intents)
 
 
 def set_constants_from_env_variables():
+    """Method to set environment variables."""
     global DISCORD_TOKEN, GUILD, LOCATION_API_TOKEN
 
     env_variables = dotenv_values(".env")
@@ -32,6 +33,7 @@ def set_constants_from_env_variables():
 
 @bot.event
 async def on_ready():
+    """Print some data to the console when the bot turns on."""
     guild = discord.utils.get(bot.guilds, name=GUILD)
 
     if not guild:
@@ -43,6 +45,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
+    """Welcome message for the new member of the guild."""
     guild = member.guild
     if guild.system_channel is not None:
         to_send = f'Welcome {member.mention} to {guild.name}!'
@@ -56,9 +59,13 @@ async def hello(ctx):
     await ctx.send(message)
 
 
-# TODO: add error handling
 async def get_coordinates_data(*location):
-    async with aiohttp.ClientSession() as session:
+    """Method to make ayncronous request to fetch coordinates from the given location."""
+    if len(location) == 0:
+        raise Exception("provide at least one parameter for location")
+
+    timeout = aiohttp.ClientTimeout(total=60)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
         base_url = "http://api.positionstack.com/v1/forward"
         location_string = " ".join(location)
         params = {
@@ -69,13 +76,20 @@ async def get_coordinates_data(*location):
         }
 
         async with session.get(base_url, params=params) as response:
-            if response.status == 200:
-                json_response = await response.json()
+            json_response = await response.json()
+
+            if response.status == 200 and json_response["data"]:
                 data = json_response["data"]
                 latitude, longitude, timezone_offset_sec = data[0]["latitude"], data[0]["longitude"], data[0]["timezone_module"]["offset_sec"]
             else:
-                response.raise_for_status()
-    
+                error_message = "Some Error!"
+                if 'error' in json_response:
+                    error_message = f"{json_response['error']['message']}. {json_response['error']['context']['query']['message']}."
+                elif "data" in json_response and not json_response["data"]:
+                    error_message = "There are no results given the location!"
+                
+                raise Exception(error_message)
+
     timezone_offset_h = int(timezone_offset_sec)//3600
 
     return latitude, longitude, timezone_offset_h
@@ -83,7 +97,7 @@ async def get_coordinates_data(*location):
 
 @bot.command(name="coordinates")
 async def get_coordinates(ctx, *location):
-    """Get the coordinates of the location"""
+    """Get the coordinates of the given location as a parameter."""
 
     latitude, longitude, timezone_offset_h = await get_coordinates_data(*location)
     message = f"Given location: {' '.join(location)}.\nLatitude = {latitude}, longitude = {longitude}, timezone = {timezone_offset_h} hours"
@@ -91,14 +105,11 @@ async def get_coordinates(ctx, *location):
     await ctx.send(message)
 
 
-# TODO: add error handling
-@bot.command(name="weather")
-async def get_weather(ctx, *location):
-    """Get the weather of the location"""
+async def get_weather_data(latitude, longitude, timezone_offset_h):
+    """Method to make ayncronous request to fetch weather data from the given coordinates."""
 
-    latitude, longitude, timezone_offset_h = await get_coordinates_data(*location)
-
-    async with aiohttp.ClientSession() as session:
+    timeout = aiohttp.ClientTimeout(total=60)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
         base_url = "https://api.open-meteo.com/v1/forecast"
         params = {
             "latitude": latitude,
@@ -107,14 +118,30 @@ async def get_weather(ctx, *location):
         }
 
         async with session.get(base_url, params=params) as response:
+            json_response = await response.json()
+            print(json_response)
+
             if response.status == 200:
-                json_response = await response.json()
                 temperature = json_response["current_weather"]["temperature"]
                 windspeed = json_response["current_weather"]["windspeed"]
                 winddirection = json_response["current_weather"]["winddirection"]
                 time = json_response["current_weather"]["time"]
             else:
-                response.raise_for_status()
+                error_message = "Some Error!"
+                if 'error' in json_response:
+                    error_message = f"{json_response['reason']}."
+
+                raise Exception(error_message)
+
+    return temperature, windspeed, winddirection, time
+
+
+@bot.command(name="weather")
+async def get_weather(ctx, *location):
+    """Get the weather of the given location as a parameter."""
+
+    latitude, longitude, timezone_offset_h = await get_coordinates_data(*location)
+    temperature, windspeed, winddirection, time = await get_weather_data(latitude, longitude, timezone_offset_h)
 
     location_string = " ".join(location)
 
@@ -122,6 +149,14 @@ async def get_weather(ctx, *location):
         f"Current weather = {temperature} °C, windspeed = {windspeed} km/h, winddirection = {winddirection}, UTC time slot = {time}."
 
     await ctx.send(message)
+
+
+@get_coordinates.error
+@get_weather.error
+async def print_error(ctx, error):
+    error_message = f"Error occurred: {error} Try again!"
+    
+    await ctx.send(error_message)
 
 
 if __name__ == "__main__":
